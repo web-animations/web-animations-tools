5597ca6a3ee045b121ce4199e89623c34276d8f3
Becky Armstrong <beckyarmstrong@google.com>
2014-02-03 15:10:41 +1100

    Made some wat-ace-editor properties customisable

--- wat-code-editor/wat-code-editor.js
+++ wat-code-editor/wat-code-editor.js
@@ -20,6 +20,18 @@ Polymer('wat-code-editor', {
   previewJavascript: '',
   html: '',
   css: '',
+  theme: 'github',
+  themeOptions: ['ambiance', 'chaos', 'chrome', 'clouds', 'clouds_midnight',
+      'cobalt', 'crimson_editor', 'dawn', 'dreamweaver', 'eclipse', 'github',
+      'idle_fingers', 'katzenmilch', 'kr_theme', 'kuroir', 'merbivore',
+      'merbivore_soft', 'mono_industrial', 'monokai', 'pastel_on_dark',
+      'solarized_dark', 'solarized_light', 'terminal', 'textmate', 'tomorrow',
+      'tomorrow_night', 'tomorrow_night_blue', 'tomorrow_night_bright',
+      'tomorrow_night_eighties', 'twilight', 'vibrant_ink', 'xcode'],
+  showGutter: true,
+  tabSize: 2,
+  useWrapMode: true,
+  showPrintMargin: true,
 
   ready: function() {
     this.selected = ['javascript', 'html', 'css'];
